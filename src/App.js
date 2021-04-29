7654918654e29b9d9bbf1f5b62b634a7d75f3773
Maymaher <mai.maher003@gmail.com>
2021-04-28 22:12:39 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+// import Longpulling from './longpolling.js';
+import ShortPolling from './shortpolling';
+
+// const  App = () => <Longpulling/>;
+const  App = () => <ShortPolling/>;
+
+export default App;
